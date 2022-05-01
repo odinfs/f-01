64ede9fa31911a8c1b5471f608e0a3562635cc27
odinfs <72218018+odinfs@users.noreply.github.com>
2022-05-01 16:46:45 +0200

    move client to /client

--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ app.get("/api/movies", (req, res, next) => {
     res.json(MOVIES);
 });
 
-app.use(express.static(path.resolve("../dist")));
+app.use(express.static(path.resolve("..", "client", "dist")));
 app.use((req, res) => {
-    res.sendFile(path.resolve("..",  "dist", "index.html"));
+    res.sendFile(path.resolve("..", "client", "dist", "index.html"));
 })
 
 const server = app.listen(3000, () => {
