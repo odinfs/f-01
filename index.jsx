7e6e53034e8b998c13d526719a4b70fa66d7509d
odinfs <72218018+odinfs@users.noreply.github.com>
2022-05-01 14:08:13 +0200

    Navigate with react router

--- index.jsx
+++ index.jsx
@@ -1,7 +1,28 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<h1>A very awesome film database</h1>,
+function FrontPage() {
+    return <div><h1>A very awesome film database</h1>
+        <ul>
+            <li><Link to="/movies">List movies</Link></li>
+            <li><Link to="/movies/new/">New movies</Link></li>
+        </ul>
+    </div>
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<FrontPage />}></Route>
+            <Route path="/movies/new" element={<h1>NewMovie</h1>}></Route>
+            <Route path="/movies" element={<h1>List Movies</h1>}></Route>
+        </Routes>
+    </BrowserRouter>
+}
+
+ReactDOM.render(
+    <Application/>,
     document.getElementById("app")
 
 );
